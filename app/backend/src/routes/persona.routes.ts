45a5061edfbdc987c8b08a9612ea2f42bc4d49bc
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-04-18 19:56:33 -0600

    manejo de permisos con endpoints para la cracion de grupos

--- app/backend/src/routes/persona.routes.ts
+++ app/backend/src/routes/persona.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getMinistros } from '../controllers/persona.controller';
+import { getMinistros, getCoordinadoresGrupo } from '../controllers/persona.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.use(authMiddleware);
 router.get('/', getMinistros);
+router.get('/coordinadores-grupo', getCoordinadoresGrupo);
 
 export default router;
